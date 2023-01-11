679f16c9e96fed36c19963325ab9a949a943dbee
Aravind S Kumar <aravindskumar@Aravinds-MacBook-Air.local>
2023-01-11 07:48:53 -0500

    updated pt file

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from PIL import Image, ImageOps
 import numpy as np
 import torch
 from torchvision import transforms
+import torch.nn.functional as F
  
 st.write("""
-         # MosquitoNet Classification
+         # MosquitoNet Classification V2
          """
          )
 
@@ -14,8 +15,8 @@ device=torch.device("cpu")
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model= torch.load('model/Species.pt', map_location = 'cpu')
-  model = model.module.to(device)
+  model= torch.load('model/nonparallelmodel.pt', map_location = 'cpu')
+  model = model.to(device)
   return model
 
 with st.spinner('Model is being loaded..'):
@@ -23,32 +24,44 @@ with st.spinner('Model is being loaded..'):
 #   model.cpu()
 #   torch.save(model, 'sp_cpu.pt')
  
-file = st.file_uploader("Upload the image to be classified U0001F447", type=["jpg", "png"])
+file = st.file_uploader("Upload the image to be classified", type=["jpg", "png"])
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-mosquito_transforms = transforms.Compose([
-    #transforms.ToPILImage(),
-    transforms.Resize([300,300]),
-    transforms.ToTensor(),
-    #transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-])
+basicTrans = transforms.Compose([ 
+                                transforms.Resize([300,300]),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+                                ])    
 
 
 
-def load_image(image):
-    image = mosquito_transforms(image)
-    
+def preprocess_image(image):
+    image = basicTrans(image)
     return image
  
 species_all = ["An. funestus",
                 "An. gambiae",
                 "An. other",
                 "Culex",
-                "Other",
-                "An. stephensi"]
+                "Other"]
 
 def upload_predict(upload_image, model):
-    inputs = load_image(upload_image)
+    inputs = preprocess_image(upload_image)
+    img_tensor = inputs.unsqueeze(0)
+
+    # Run the model
+    output = model(img_tensor)
+    
+    # get softmax of output
+
+    output = F.softmax(output, dim=1)
+
+    probab, pred = torch.max(output, 1)
+    print(output, pred, probab, probab.item())
+    pred_class = pred.item()
+    probab_value = probab.item()
+    return pred_class, probab_value
+
     probabilities = model(inputs.unsqueeze(0).to(device))
 
     _, predicted = torch.max(probabilities.data, 1)
@@ -59,10 +72,19 @@ if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
-    st.image(image, use_column_width=True)
-    label = upload_predict(image, model)
+
+    # Open the image
+    image_disp = image.copy()
+
+    # Resize the image
+    max_size = (400, 400)
+    image_disp.thumbnail(max_size)
+
+    st.image(image_disp, use_column_width= False)
+    label, score = upload_predict(image, model)
     image_class = label #'mosquito' #str(predictions[0][0][1])
-    score= 0.9 #np.round(predictions[0][0][2]) 
-    st.write("The image is classified as",image_class)
-    # st.write("The similarity score is approximately",score)
+    st.write("### The image is classified as",species_all[image_class])
+    st.write(f"#### The similarity score is approximately : {score*100:.2f} % ")
+    # if score < 0.8:
+    #     st.write("##### Confidence is below 80% - please upload a clearer image")
     print("The image is classified as ",image_class) #, "with a similarity score of",score)
