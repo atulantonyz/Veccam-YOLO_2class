4f2b4c40307bec4ba77c372a22db2436d3043779
aravind <aravind@pop-os.localdomain>
2023-03-17 19:44:27 -0400

    Reverted to version field tested in Jan 23

--- app.py
+++ app.py
@@ -5,31 +5,22 @@ import numpy as np
 import torch
 from torchvision import transforms
 import torch.nn.functional as F
-# import mysql.connector
  
 st.write("""
-         # MosquitoNet Classification V4
-         ### Hierarchical classification of mosquitos - An . stephensi
+         # MosquitoNet Classification - Jan 23
          """
          )
 
 device=torch.device("cpu")
 
 @st.cache(allow_output_mutation=True)
-def load_genus_model():
-  model= torch.load('model/genus_model.pt', map_location = 'cpu')
-  model = model.to(device)
-  return model
-
-@st.cache(allow_output_mutation=True)
-def load_species_model():
-  model= torch.load('model/steph_onevsall.pt', map_location = 'cpu')
+def load_model():
+  model= torch.load('model/nonparallelmodel.pt', map_location = 'cpu')
   model = model.to(device)
   return model
 
 with st.spinner('Model is being loaded..'):
-  genus_model=load_genus_model()
-  species_model=load_species_model()
+  model=load_model()
  
 file = st.file_uploader("Upload the image to be classified", type=["jpg", "png"])
 st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -41,37 +32,16 @@ basicTrans = transforms.Compose([
                                 ])    
 
 
+
 def preprocess_image(image):
     image = basicTrans(image)
     return image
-
-genus_all = ["Anopheles","Culex","Other"]
  
-species_all = ["Not Stephensi", "Stephensi"]
-
-
-
-# def save_to_database(image_file, label, probab):
-#     # Connect to the database
-#     print('trying to insert into database')
-#     connection = mysql.connector.connect(
-#         host=st.secrets["host"],
-#         user=st.secrets["user"],
-#         password=st.secrets["password"],
-#         database=st.secrets["database"]
-#     )
-
-#     cursor = connection.cursor()
-#     print('connected to database')
-#     # Insert the data into the table
-#     insert_command = f"INSERT INTO `app_runs` (`id`, `imagename`, `prediction`, `probability`, `datetime`) VALUES ('', '{image_file}', '{label}', '{probab}' , CURRENT_TIMESTAMP);"
-    
-#     cursor.execute(insert_command)
-
-#     connection.commit()
-#     print('inserted into database')
-#     cursor.close()
-#     connection.close()
+species_all = ["An. funestus",
+                "An. gambiae",
+                "An. other",
+                "Culex",
+                "Other"]
 
 def upload_predict(upload_image, model):
     inputs = preprocess_image(upload_image)
@@ -85,13 +55,19 @@ def upload_predict(upload_image, model):
     output = F.softmax(output, dim=1)
 
     probab, pred = torch.max(output, 1)
-    # print(output, pred, probab, probab.item())
+    print(output, pred, probab, probab.item())
     pred_class = pred.item()
     probab_value = probab.item()
     return pred_class, probab_value
 
+    probabilities = model(inputs.unsqueeze(0).to(device))
+
+    _, predicted = torch.max(probabilities.data, 1)
+    pred_arr = predicted.cpu().detach().numpy()
+    return species_all[pred_arr[0]]
+
 if file is None:
-    st.text("""Please upload an image file!""")
+    st.text(" ###### Please upload an image file!")
 else:
     image = Image.open(file)
 
@@ -103,12 +79,10 @@ else:
     image_disp.thumbnail(max_size)
 
     st.image(image_disp, use_column_width= False)
-    genus_index, genus_score = upload_predict(image, genus_model)
-    # save_to_database(file.name, species_all[image_class], score*100)
-    st.write("### The image is classified into Genus : ",genus_all[genus_index])
-    st.write(f"#### The similarity score is approximately : {genus_score*100:.2f} % ")
-
-    if genus_index == 0:
-      species_index, species_score = upload_predict(image, species_model)
-      st.write("### The image is classified into Species : ",species_all[species_index])
-      st.write(f"#### The similarity score is approximately : {species_score*100:.2f} % ")
+    label, score = upload_predict(image, model)
+    image_class = label #'mosquito' #str(predictions[0][0][1])
+    st.write("### The image is classified as",species_all[image_class])
+    st.write(f"#### The similarity score is approximately : {score*100:.2f} % ")
+    # if score < 0.8:
+    #     st.write("##### Confidence is below 80% - please upload a clearer image")
+    print("The image is classified as ",image_class) #, "with a similarity score of",score)
