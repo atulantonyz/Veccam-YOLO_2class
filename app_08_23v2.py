5fb08ed94368303d648bd37771efec3d4d8d9e3a
atulantonyz <40585728+atulantonyz@users.noreply.github.com>
2023-08-23 14:28:45 -0400

    Update app_08_23v2.py

--- app_08_23v2.py
+++ app_08_23v2.py
@@ -79,7 +79,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 basicTrans = transforms.Compose([ 
                                 transforms.Resize([300,300]),
                                 transforms.ToTensor(),
-                                # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+                                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
                                 ])    
 
 def yolo_crop(image):
